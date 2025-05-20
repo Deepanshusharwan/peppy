53e6654947e2b66552ac8606ae7f98ea0b5d4c1f
Deepanshusharwan <deepanshusharwan35@gmail.com>
2025-05-20 13:48:53 +0530

    switched to the uv package manager

--- src/main.py
+++ src/main.py
@@ -3,11 +3,13 @@
 import os
 import sys
 import subprocess
+
 #from src.utils import app_lister
 from utils import app_lister
 
 apps = app_lister.application_lister()
+
 for m in apps:
     print(m.get("name"))
     print("")
-print(apps)
+
