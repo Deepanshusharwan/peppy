35c79504a65ae675b6e306ae1ee6ed0e19da0e0f
Deepanshu Sharwan <deepanshusharwan35@gmail.com>
2025-06-13 21:39:17 +0530

    modified the main.py to run the entire application

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import sys
+from PyQt6.QtWidgets import (
+    QWidget, QLineEdit, QLabel, QPushButton, QScrollArea, QMainWindow,
+    QApplication, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy
+    )
 
 
 #from src.utils import app_lister
 from utils import app_lister
 
+# from ui import main_window
+from ui.main_window import MainWindow, QApplication
+
 
 apps = app_lister.application_lister()
 
-
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+sys.exit(app.exec())
