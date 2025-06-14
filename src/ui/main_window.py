fd7abdee508540e4713c72a922264c512f863adc
Deepanshu Sharwan <deepanshusharwan35@gmail.com>
2025-06-14 19:40:54 +0530

    added the search ability to the gui and put auto completion in WIP

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,7 +1,7 @@
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtWidgets import (
     QWidget, QLineEdit, QLabel, QPushButton, QScrollArea, QMainWindow,
-    QApplication, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy
+    QApplication, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy, QCompleter
     )
 from PyQt6.QtCore import QObject, Qt, pyqtSignal
 from PyQt6.QtGui import QPainter, QFont, QColor, QPen
@@ -40,7 +40,16 @@ class MainWindow(QMainWindow):
         self.scroll.setWidgetResizable(True)
         self.scroll.setWidget(self.controls)
 
+        # searchbar
         self.searchbar = QLineEdit()
+        self.searchbar.textChanged.connect(self.update_display)
+
+        # TODO make the completer suggestions for autocompletion and display them as the
+        # placeholder text in searchbar and pressing tab for autocompletion
+
+        # adding the autocompleter
+        #self.completer = QCompleter(widget_names)
+        #self.completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
         
         #add the items to vboxlayout (applied to the container widget)
         # which encompasses the whole window
@@ -48,16 +57,21 @@ class MainWindow(QMainWindow):
         containerLayout = QVBoxLayout()
         containerLayout.addWidget(self.searchbar)
         containerLayout.addWidget(self.scroll)
-
         container.setLayout(containerLayout)
-        containerLayout.addWidget(self.searchbar)
-        containerLayout.addWidget(self.scroll)
 
         container.setLayout(containerLayout)
         self.setCentralWidget(container)
 
         self.setGeometry(600, 100, 800, 600)
         self.setWindowTitle("Peppy")       
+
+    def update_display(self,text):
+        
+        for widget in self.widgets:
+            if text.lower() in widget.name.lower():
+                widget.show()
+            else:
+                widget.hide()
 
 
 if __name__ == "__main__":
