d1fc36e076f3df90fb8f9ffeb1b4274f4c9d949a
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-06-15 20:10:51 +0530

    create applications.json to store app name, path and count. ! working w/ ctypes

--- src/utils/main.go
+++ src/utils/main.go
@@ -2,27 +2,31 @@ package main
 
 import "C"
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
-type Factory struct {
-	Industry string `json:"industry"`
-	Place    string `json:"place"`
+type AppInfo struct {
+	Name    string `json:"name"`
+	Path    string `json:"path"`
+	Counter int    `json:"count"`
 }
 
 // NOTE: can't directly return go maps/ slices, thus use JSON
 
 //export testjson
-func testjson() *C.char {
-	factory := Factory{Industry: "automotive", Place: "texas"}
-	jsonData, err := json.Marshal(factory)
-	if err != nil {
-		return C.CString(`{"error": "json encoding failed"}`)
-	}
-	return C.CString(string(jsonData))
-}
+// func testjson() *C.char {
+// 	factory := Factory{Industry: "automotive", Place: "texas"}
+// 	jsonData, err := json.Marshal(factory)
+// 	if err != nil {
+// 		return C.CString(`{"error": "json encoding failed"}`)
+// 	}
+// 	return C.CString(string(jsonData))
+// }
 
 //export listApplications
 func listApplications() {
@@ -33,7 +37,38 @@ func listApplications() {
 		fmt.Println("Command execution error:", err)
 		return
 	}
-	fmt.Print("applications: \n", string(output))
+	// fmt.Print("applications: \n", string(output))
+
+	var apps []AppInfo
+	scanner := bufio.NewScanner(strings.NewReader(string(output)))
+
+	for scanner.Scan() {
+		appPath := scanner.Text()
+
+		//extract app name
+		sectors := strings.Split(appPath, "/")
+		appName := strings.TrimSuffix(sectors[len(sectors)-1], ".app")
+
+		apps = append(apps, AppInfo{
+			Name: appName,
+			Path: appPath,
+		})
+	}
+
+	file, err := os.Create("applications.json")
+	if err != nil {
+		fmt.Println("Failed to create file:", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ") // pretty print
+	err = encoder.Encode(apps)
+	if err != nil {
+		fmt.Println("Failed to encode JSON:", err)
+		return
+	}
 }
-func main() {
-}
+
+func main() {}
--- src/utils/test.py
+++ src/utils/test.py
@@ -2,13 +2,15 @@ import ctypes
 import json
 
 lib=ctypes.CDLL('./main.so')
-lib.listApplications()
-lib.testjson.restype=ctypes.c_char_p
+# lib.listApplications()
+
+# set return type for testjson()
+lib.listApplications.restype=ctypes.c_char_p
 
 # calling function
-jsontest_bytes=lib.testjson()
+jsontest_bytes=lib.listApplications()
 jsontest_str=jsontest_bytes.decode('utf-8')
 
 #converting json to python dict
-data=json.loads(jsontest_str)
+data=json.loads(li)
 print(data)
