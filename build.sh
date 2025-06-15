04b03f6468a2256dfe45798232f2150fe31bcf26
Deepanshu Sharwan <deepanshusharwan35@gmail.com>
2025-06-15 12:27:55 +0530

    made some improvements in the build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 # === config ===
-APP_NAME = "Peppy"
-ICON_FILE = "peppy.svg"
-EXECUTABLE_NAME = "peppy"
-DESKTOP_FILE = "/usr/share/applications/"
+APP_NAME="Peppy"
+ICON_FILE="peppy.svg"
+EXECUTABLE_NAME="peppy"
+DESKTOP_FILE="$HOME/.local/share/applications/$APP_NAME.desktop"
+
 
 git clone "https://github.com/Deepanshusharwan/peppy"
 
-cd peppy/
+cd peppy || { echo "Failed to enter directory"; exit 1; }
 
 # === Setting up the enviroment ===
 python -m venv .venv
@@ -20,12 +21,23 @@ uv pip install .
 pip install pyinstaller
 
 # === building the exec ===
-if [[ $OSTYPE == "linux-gnu" ]]; then
+if [[ $OSTYPE == "linux-gnu"* ]]; then
   ./linux_build.sh
 
 else
-  command
+  echo "Looks like your system isn't yet supported by peppy."
+  echo "Please create an issue on the below link with you system name and version."
+  echo "https://github.com/Deepanshusharwan/peppy/issues/new"
   
 fi
+
+# === removing the repo ===
+cd ../ 
+rm -rf peppy/
+
+echo ""
+echo ""
+echo "The install is complete!"
+
 
 
