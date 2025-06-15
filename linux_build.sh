679a5852431cdaad36dfced7e95d25af5216792b
Deepanshu Sharwan <deepanshusharwan35@gmail.com>
2025-06-15 11:32:23 +0530

    add logo image and made a general build script for all platforms

--- linux_build.sh
+++ linux_build.sh
@@ -3,5 +3,6 @@
 pyinstaller --onefile \
   --name peppy \
   --windowed \
+  --icon=peppy.svg \
   --add-data ".venv/lib/python3.13/site-packages/PyQt6/Qt6/plugins/platforms:PyQt6/Qt6/plugins/platforms" \
   src/main.py
