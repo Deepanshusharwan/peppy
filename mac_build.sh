4e61953ad8e7ecef2e5e6333bbb856d86bac0ddc
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-07-16 17:09:12 +0530

    file: mac_build.sh, add app_lister.so and peppy.icns

--- mac_build.sh
+++ mac_build.sh
@@ -3,7 +3,7 @@
 
 # === config ===
 APP_NAME="Peppy"
-ICON_FILE="peppy.svg"
+ICON_FILE="assets/peppy.icns"
 EXECUTABLE_NAME="peppy"
 DESKTOP_FILE="$HOME/.local/share/applications/$APP_NAME.desktop"
 
@@ -17,4 +17,5 @@ pyinstaller --onedir --clean --strip \
   --windowed \
   --icon="$ICON_FILE" \
   --add-data "src/JetBrainsMonoNerdFont-Bold.ttf:." \
+  --add-data "src/utils/app_lister_lib/mac/app_lister.so:." \
   src/main.py
